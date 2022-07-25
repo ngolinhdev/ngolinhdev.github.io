1ab69e4fba325b6a6c170aa1027a137b3e33a1bc
qnreck <qnreck>
2022-07-26 00:52:55 +0700

    1st

---
+++ scripts/main.js
@@ -0,0 +1,208 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000,
+        function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
+    }
+  }
+});
+
+// Photo Filter
+var activeFilter = "all";
+
+$(".ww-filter-button").on("click", function(e) {
+  // remove btn-primary from all buttons first
+  $(".ww-filter-button").removeClass("btn-primary");
+  $(".ww-filter-button").addClass("btn-outline-primary");
+
+  // add btn-primary to active button
+  var button = $(this);
+  button.removeClass("btn-outline-primary");
+  button.addClass("btn-primary");
+  filterItems(button.data("filter"));
+  e.preventDefault();
+});
+
+function filterItems(filter) {
+  if (filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $(".ww-gallery .card").each(function() {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if (filter === "all") {
+      show = true;
+    } else {
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+    setTimeout(function() {
+      if (show && !card.is(":visible")) {
+        card.fadeIn(400);
+      }
+    }, 500);
+  });
+}
+
+// Light Box
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+
+	// CountDown Js
+	var deadline = 'September 4 2022 10:30:00 GMT+0700';
+		function time_remaining(endtime){
+			var t = Date.parse(endtime) - Date.parse(new Date());
+			var seconds = Math.floor( (t/1000) % 60 );
+			var minutes = Math.floor( (t/1000/60) % 60 );
+			var hours = Math.floor( (t/(1000*60*60)) % 24 );
+			var days = Math.floor( t/(1000*60*60*24) );
+			return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+		}
+		function run_clock(id,endtime){
+			var clock = document.getElementById(id);
+			
+			// get spans where our clock numbers are held
+			var days_span = clock.querySelector('.days');
+			var hours_span = clock.querySelector('.hours');
+			var minutes_span = clock.querySelector('.minutes');
+			var seconds_span = clock.querySelector('.seconds');
+
+			function update_clock(){
+				var t = time_remaining(endtime);
+				
+				// update the numbers in each part of the clock
+				days_span.innerHTML = t.days;
+				hours_span.innerHTML = ('0' + t.hours).slice(-2);
+				minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
+				seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
+				
+				if(t.total<=0){ clearInterval(timeinterval); }
+			}
+			update_clock();
+			var timeinterval = setInterval(update_clock,1000);
+		}
+		run_clock('clockdiv',deadline);	
+
+    // Testimonial Slider Js
+	$('.wd_testimonial_slider .owl-carousel').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:false,
+		autoplay:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+	
+
+  // Back to Top js
+	$(window).scroll(function(){ 
+    if ($(this).scrollTop() > 100) { 
+        $('#scroll').fadeIn(); 
+    } else { 
+        $('#scroll').fadeOut(); 
+    } 
+}); 
+
+$('#scroll').on("click", function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+}); 
+
+
+	// Menu js for Position fixed
+	$(window).scroll(function(){
+		var window_top = $(window).scrollTop() + 1; 
+		if (window_top > 500) {
+			$('.wd_header_wrapper').addClass('menu_fixed animated fadeInDown');
+		} else {
+			$('.wd_header_wrapper').removeClass('menu_fixed animated fadeInDown');
+		}
+	});
+
+  	//Single page scroll js
+	$('.wd_single_index_menu_rsvp a').on('click' , function(e){
+	  $('.wd_single_index_menu_rsvp a').removeClass('active');
+	  $(this).parent().addClass('active');
+	  var target = $('[section-scroll='+$(this).attr('href')+']');
+	  e.preventDefault();
+	  var targetHeight = target.offset().top-parseInt('83', 10);
+	  $('html, body').animate({
+	   scrollTop: targetHeight
+	  }, 1000);
+	});
+	
+	$(window).scroll(function() {
+	  var windscroll = $(window).scrollTop();
+	  var target = $('.wd_single_index_menu_rsvp a');
+	  if (windscroll >= 0) {
+	   $('[section-scroll]').each(function(i) {
+		if ($(this).position().top <= windscroll + 83) {
+		 target.removeClass('active');
+		 target.eq(i).addClass('active');
+		}
+	   });
+	  }else{
+	   target.removeClass('active');
+	   $('.wd_single_index_menu_rsvp a').addClass('active');
+	  }
+
+	});
+
+	
